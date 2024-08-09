9579d7089d6543327149efa0bd89d447201e84aa
Hendrik Roskamp <hendrik@roskamp.me>
2024-08-09 11:55:48 +0200

    fixed tests, changed workflow etc.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'Milsymbol-Angular-Showcase' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Milsymbol-Angular-Showcase');
+    expect(app.title).toEqual('Milsymbol-Showcase');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Milsymbol-Angular-Showcase');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Milsymbol-Showcase!');
   });
 });
