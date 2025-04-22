3754093273824c3ee2fb06c54013843588e87672
Hendrik Roskamp <hendrik.roskamp@outlook.de>
2025-04-22 18:30:12 +0200

    ... update again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,8 @@ import std2525c from 'milsymbol';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+    styleUrl: './app.component.scss',
+    standalone: false
 })
 export class AppComponent {
   title = 'Milsymbol-Showcase';
